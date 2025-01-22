c93b74bcbf98365260ed3d7311c16df07aa36f90
Bruh <nazarenko-zhora@mail.ru>
2025-01-22 18:09:05 +0700

    New application properties entries have been added, as well as scheme.sql and data.sql files for initializing mysql table.

---
+++ quotesender/src/main/resources/db/mysql/data.sql
@@ -0,0 +1,5 @@
+
+INSERT IGNORE INTO Users(userID,login,password) VALUES (1,'Ivan', '1234');
+
+INSERT IGNORE INTO UsersQuote(quoteID,quoteText,userID) VALUES (1,'i wrote some quote to test a thing',1);
+
